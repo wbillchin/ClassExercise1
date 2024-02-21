64a29f65dc77c353822b85e0b213c86ebf7b7768
Bill Chin <chinww@vcu.edu>
2024-02-21 14:57:49 -0500

    changed to using array of ints for birthyear and age

--- ClassExercise1/Program.cs
+++ ClassExercise1/Program.cs
@@ -6,6 +6,7 @@
 
 
 using System;
+using System.Diagnostics;
 
 namespace MyApp
 {
@@ -15,52 +16,64 @@ namespace MyApp
         static void Main(string[] args)
         {
             string inputString;
+            const int maxBirthYearsCount = 10;
+            int[] birthYears = new int[maxBirthYearsCount];
+            int[] age = new int[maxBirthYearsCount];
+            int birthYearIndex = 0;
+            bool finishedInputs = false;
 
-            Console.WriteLine("Type in a birth year: ");
-
-            // reads in a string from the console
-            inputString = Console.ReadLine();
-
-            // checks that we got a string back instead of a null
-            // note we have not covered null yet
-            if (inputString == null)
+            while( finishedInputs == false )
             {
-                Console.WriteLine("Incorrect input, exiting");
-                return;
+                Console.WriteLine("Type in a birth year or \"done\" to finish: ");
+                // reads in a string from the console
+                inputString = Console.ReadLine();
+
+                // checks that we got a string back instead of a null
+                // note we have not covered null yet
+                if (inputString == null)
+                {
+                    Console.WriteLine("Incorrect input, exiting");
+                    return;
+                }
+
+                if (inputString.Equals("done") == true) {
+                    finishedInputs = true;
+                } else {
+
+                    birthYears[birthYearIndex] = Convert.ToInt32(inputString);
+
+                    // increment the birth year index for each time through the loop
+                    birthYearIndex++;
+
+                    // check to see if we are out of slots in the array
+                    if( birthYearIndex > maxBirthYearsCount - 1 )
+                    {
+                        finishedInputs = true;
+                    }
+
+                }
             }
 
-            int firstBirthYear = Convert.ToInt32(inputString);
+            int receivedInputsCount = birthYearIndex;
 
-            Console.WriteLine("Type in a birth year: ");
+            // reset the birthYear counter to use it again
+            birthYearIndex = 0;
 
-            // reads in a string from the console
-            inputString = Console.ReadLine();
-
-            // checks that we got a string back instead of a null
-            // note we have not covered null yet
-            if (inputString == null)
+            for( birthYearIndex = 0; birthYearIndex < receivedInputsCount; birthYearIndex++ )
             {
-                Console.WriteLine("Incorrect input, exiting");
-                return;
+                age[birthYearIndex] = CalculateAge(birthYears[birthYearIndex]);
             }
-
-            int secondBirthYear = Convert.ToInt32(inputString);
-
-
-            int firstAge = CalculateAge(firstBirthYear);
-            int secondAge = CalculateAge(secondBirthYear);
-
 
             /*
              * note that below, the " is escaped as \" such that the compiler
              * doesn't mistake it for the end of the string
              */
-            Console.WriteLine("First age is " + firstAge);
-            Console.WriteLine("Second age is " + secondAge);
+            Console.WriteLine("First age is " + age[0]);
+            Console.WriteLine("Second age is " + age[1]);
 
-            double averageAge = CalculateAverageAge(firstAge, secondAge);
+            //double averageAge = CalculateAverageAge(firstAge, secondAge);
 
-            Console.WriteLine("Average age is " + averageAge);
+            //Console.WriteLine("Average age is " + averageAge);
 
         }
 
